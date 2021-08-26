97990e81a859c0caa41db1fc6fd02c093b049782
Buddya Tsebekov <tsebekov94@gmail.com>
2021-08-26 20:29:09 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener errorListener = i -> i == 33;
+
+        Worker worker = new Worker(listener, errorListener);
+        worker.start();
+    }
+}
